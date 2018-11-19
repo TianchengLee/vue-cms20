3d78729d6b0df9932812cdfaba6bbd667fcf377d
TianchengLee <ltc6634284@gmail.com>
2018-11-19 15:34:58 +0800

    完成首页轮播图的样式修改

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router/'
 Vue.config.productionTip = false
 
 // 按需导入需要的mint-ui组件
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+
 // 注册组件
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 // 导入mui的样式
 import './lib/mui/css/mui.css'
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home-container">
-    HomeContainer
+    <!-- 轮播图 -->
+    <mt-swipe class="binner-container" :auto="4000">
+      <mt-swipe-item>1</mt-swipe-item>
+      <mt-swipe-item>2</mt-swipe-item>
+      <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
   </div>
 </template>
 
@@ -8,6 +13,22 @@
   
 </script>
 
-<style>
+<style lang="less">
+.home-container {
+  .binner-container {
+    height: 200px;
 
+    .mint-swipe-item {
+      &:nth-child(1) {
+        background-color: yellow;
+      }
+      &:nth-child(2) {
+        background-color: blue;
+      }
+      &:nth-child(3) {
+        background-color: skyblue;
+      }
+    }
+  }
+}
 </style>

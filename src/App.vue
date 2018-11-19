379cb455305be77a71c5851f97d5c0c3fd20f252
TianchengLee <ltc6634284@gmail.com>
2018-11-19 14:32:42 +0800

    tabBar路由选中高亮

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,24 @@
 
     <!-- 底部tabBar -->
     <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-email"></span>
 				<span class="mui-tab-label">会员</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
 				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
 					<span class="mui-badge">0</span>
 				</span>
 				<span class="mui-tab-label">购物车</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
-			</a>
+			</router-link>
 		</nav>
     
     <!-- 中间内容区域 -->
